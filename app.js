c543a0c34171c23bb3c27e0ebdd3024f277b42da
Camilla Rees <camilla.c.rees@gmail.com>
2022-07-27 14:58:47 -0600

    added random number function

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ let seattle = {
   minCust: 23,
   maxCust: 65,
   avgSale: 6.3,
-  getHeight: function () {
-    console.log(this.height);
-  },
-  getRandomNumber: function (minCust, maxCust) {
+  randomNumberCust: function () {
     console.log(this);
   },
 };
@@ -16,35 +13,25 @@ let tokyo = {
     minCust: 3,
     maxCust: 24,
     avgSale: 1.2,
-    getHeight: function () {
-      console.log(this.height);
-    },
-    getRandomNumber: function (minCust, maxCust) {
-      console.log(this);
-    },
-  };
-
+    randomNumberCust: function () {
+        console.log(this);
+      },
+    };
 
 let dubai = {
     minCust: 11,
     maxCust: 38,
     avgSale: 3.7,
-    getHeight: function () {
-      console.log(this.height);
-    },
-    getRandomNumber: function (minCust, maxCust) () {
-      console.log(this);
-    },
-  };
+    randomNumberCust: function () {
+        console.log(this);
+      },
+    };
 
 let paris = {
   minCust: 20,
   maxCust: 28,
   avgSale: 2.3,
-  getHeight: function () {
-    console.log(this.height);
-  },
-  getRandomNumber: function (minCust, maxCust) () {
+  randomNumberCust: function () {
     console.log(this);
   },
 };
@@ -53,31 +40,41 @@ let lima = {
     minCust: 2,
     maxCust: 16,
     avgSale: 4.6,
-    getHeight: function () {
-      console.log(this.height);
-    },
-    getRandomNumber: function (minCust, maxCust) () {
-      console.log(this);
-    },
-  };
+    randomNumberCust: function () {
+        console.log(this);
+      },
+    };
 
-function getRandomNumber(min, max) {
+
+function randomNumberCust(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min );
 }
 
-let random = getRandomNumber();
+randomNumberCust (minCust, maxCust);
+
+// let hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
+
+// function cookiesPerHour () {
+
+//     for (let i = 0; i <= hours.length; i++) {
+//         let li = document.createElement('li');
+//         li.innerText = hours[i];
+//         list.append(li);
+
+// }
+// }
+// cookiesPerHour();
 
 
-let hours = ["6am:", "7am:", "8am:", "9am:", "10am:", "11am:", "12pm:", "1pm:", "2pm:", "3pm:", "4pm:", "5pm:", "6pm:", "7pm:"];
-let cookies = []
-function cookiesPerHour (); {
+// let totalHours = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    for (let i = 0; i <= hours.length; i++) {
-        let li = document.createElement('li');
-        li.innerText = hours[i];
-        list.appendChild(li);
+// function sumTotal(sumArr) {    
+//   let sumAB = sumTotal(sumArr[0], sumArr[1])[0];
+//   let sumABC = sumTotal(sumAB, sumArr[2])[0];
+//   let result = [sumABC, string5];
+//   console.log(sumABC);
+//   return result;
+// }
 
-}
-}
-
+// testSumArray(totalHours);
 
