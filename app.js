f94314c1de23ce3bef8d50c3f1a2e44b2cab5315
Camilla Rees <camilla.c.rees@gmail.com>
2022-07-30 15:26:13 -0600

    added constructors and functions

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 let locations = ['seattle', 'tokyo', 'dubai', 'paris', 'lima'];
 let hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
-let allLocations = [];
 // locationTotals = [];
 
 function randomNumberCust(min, max) {
